f4676caf629d7b989b90e4774750dd686aadbf44
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2019-06-20 15:32:56 -0300

    Update Pessoa.java
    Classe abstrata pessoa

--- ec05_datacar/src/main/java/br/com/ap220191/ec05_datacar/model/source/Pessoa/Pessoa.java
+++ ec05_datacar/src/main/java/br/com/ap220191/ec05_datacar/model/source/Pessoa/Pessoa.java
@@ -1,4 +1,50 @@
-package Pessoa;
+package java.br.com.ap220191.ec05_datacar.model.Pessoa;
+import java.util.Date;
 
-public class Pessoa {
+public abstract class Pessoa {
+	private String nome;
+	private String cpf;
+	private Date dataNas;
+	private String sexo;
+	private String email;
+	private long telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNas() {
+		return dataNas;
+	}
+	public void setDataNas(Date dataNas) {
+		this.dataNas = dataNas;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+	
+	
 }
+

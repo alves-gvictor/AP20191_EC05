be7bbd6373a9a898863aeb199c38f368141a4071
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2019-06-20 15:33:32 -0300

    Update FuncionarioVendedor.java
    Classe Funcionario vendedor

--- ec05_datacar/src/main/java/br/com/ap220191/ec05_datacar/model/source/Pessoa/FuncionarioVendedor.java
+++ ec05_datacar/src/main/java/br/com/ap220191/ec05_datacar/model/source/Pessoa/FuncionarioVendedor.java
@@ -1,4 +1,39 @@
-package Pessoa;
+package java.br.com.ap220191.ec05_datacar.model.Pessoa;
 
-public class FuncionarioVendedor {
+public class FuncionarioVendedor extends Pessoa{
+	private int matricula;
+	private int salario;
+	private double comissao;
+	private double valorVendido;
+	
+	
+	public void calcularSalario{
+		comissao = (valorVendido*15/100);
+	}
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public int getSalario() {
+		return salario;
+	}
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+	public double getComissao() {
+		return comissao;
+	}
+	public void setComissao(double comissao) {
+		this.comissao = comissao;
+	}
+	public double getValorVendido() {
+		return valorVendido;
+	}
+	public void setValorVendido(double valorVendido) {
+		this.valorVendido = valorVendido;
+	}
+
 }

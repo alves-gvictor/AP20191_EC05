4fcc55029a094d0655576bdbfedb75ddd034d1cd
gabriel-freitas-melo <48661085+gabriel-freitas-melo@users.noreply.github.com>
2019-06-20 18:15:30 -0300

    essa é a parte do veiculo pretendido
    veiculo pretendido e seus metodos

---
+++ Veiculo_pretendido.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Veiculo;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Veiculo_pretendido {
+
+    //Atributos
+   private String cor;
+   private String modelo;
+   private String marca;
+   private double valor;
+   private String placa;
+   
+   //Metodos
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+   
+   
+}
